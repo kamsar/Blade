01f035f67c05b4b80d787f5ba9efd9b58968dd44
Sabatino DeRico <tderico@geekhive.com>
2017-08-30 15:10:56 -0400

    Removed refernce to buckets and updated page editor references to be experience editor references

--- Source/Blade/Pipelines/ResolveRenderingDataSource/HandleSearch.cs
+++ Source/Blade/Pipelines/ResolveRenderingDataSource/HandleSearch.cs
@@ -1,59 +1,58 @@
-﻿using System.Linq;
-using Sitecore.Buckets.Util;
-using Sitecore.ContentSearch;
+﻿using Sitecore.ContentSearch;
 using Sitecore.ContentSearch.SearchTypes;
 using Sitecore.ContentSearch.Utilities;
 using Sitecore.Data.Items;
+using System.Linq;
 
 namespace Blade.Pipelines.ResolveRenderingDataSource
 {
-	/// <summary>
-	/// Handles data sources that are defined using index search syntax
-	/// TODO: when Update-2 comes out, supposedly the UIFilterHelpers.ParseDatasourceString will become generic.
-	/// If that happens, this pipeline should be refactored to allow returning a strongly typed data source item in addition to
-	/// the Item result so as to avoid having to pre-convert search results to Items.
-	/// </summary>
-	public class HandleSearch : ResolveRenderingDataSourcePipelineProcessor
-	{
-		public override void DoProcess(ResolveRenderingDataSourceArgs args)
-		{
-			// if a search context came in, we don't dispose it when done - otherwise we dispose our temp context
-			bool disposeSearchContext = args.SearchContext == null;
+    /// <summary>
+    /// Handles data sources that are defined using index search syntax
+    /// TODO: when Update-2 comes out, supposedly the UIFilterHelpers.ParseDatasourceString will become generic.
+    /// If that happens, this pipeline should be refactored to allow returning a strongly typed data source item in addition to
+    /// the Item result so as to avoid having to pre-convert search results to Items.
+    /// </summary>
+    public class HandleSearch : ResolveRenderingDataSourcePipelineProcessor
+    {
+        public override void DoProcess(ResolveRenderingDataSourceArgs args)
+        {
+            // if a search context came in, we don't dispose it when done - otherwise we dispose our temp context
+            bool disposeSearchContext = args.SearchContext == null;
 
-			IProviderSearchContext searchContext = null;
-			try
-			{
-				searchContext = args.SearchContext ?? ContentSearchManager.CreateSearchContext(new SitecoreIndexableItem(args.ContextItem));
+            IProviderSearchContext searchContext = null;
+            try
+            {
+                searchContext = args.SearchContext ?? ContentSearchManager.CreateSearchContext(new SitecoreIndexableItem(args.ContextItem));
 
-				var query = CreateQuery(searchContext, args.DataSource);
+                var query = CreateQuery(searchContext, args.DataSource);
 
-				args.DataSourceItems.AddRange(ProcessQueryResults(query));
-			}
-			finally
-			{
-				if(disposeSearchContext && searchContext != null) searchContext.Dispose();
-			}
-		}
+                args.DataSourceItems.AddRange(ProcessQueryResults(query));
+            }
+            finally
+            {
+                if (disposeSearchContext && searchContext != null) searchContext.Dispose();
+            }
+        }
 
-		protected virtual IQueryable<SitecoreUISearchResultItem> CreateQuery(IProviderSearchContext context, string query)
-		{
-			var parsedQuery = UIFilterHelpers.ParseDatasourceString(query);
-			var linqQuery = LinqHelper.CreateQuery(context, parsedQuery);
+        protected virtual IQueryable<SitecoreUISearchResultItem> CreateQuery(IProviderSearchContext context, string query)
+        {
+            var parsedQuery = SearchStringModel.ParseDatasourceString(query);
+            var linqQuery = LinqHelper.CreateQuery(context, parsedQuery);
 
-			return FilterQuery(linqQuery);
-		}
+            return FilterQuery(linqQuery);
+        }
 
-		protected virtual IQueryable<SitecoreUISearchResultItem> FilterQuery(IQueryable<SitecoreUISearchResultItem> query)
-		{
-			string language = Sitecore.Context.Language.CultureInfo.TwoLetterISOLanguageName;
+        protected virtual IQueryable<SitecoreUISearchResultItem> FilterQuery(IQueryable<SitecoreUISearchResultItem> query)
+        {
+            string language = Sitecore.Context.Language.CultureInfo.TwoLetterISOLanguageName;
 
-			// this makes us get "normal" query behavior of returning context language and latest version only
-			return query.Where(x => x.Language == language && x["_latestversion"] == "1");
-		}
+            // this makes us get "normal" query behavior of returning context language and latest version only
+            return query.Where(x => x.Language == language && x["_latestversion"] == "1");
+        }
 
-		protected virtual Item[] ProcessQueryResults(IQueryable<SitecoreUISearchResultItem> query)
-		{
-			return query.Select(x => x.GetItem()).ToArray();
-		}
-	}
+        protected virtual Item[] ProcessQueryResults(IQueryable<SitecoreUISearchResultItem> query)
+        {
+            return query.Select(x => x.GetItem()).ToArray();
+        }
+    }
 }
